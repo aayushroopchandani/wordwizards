cf209322cdc6431d6cf9e0795890bce9a53e9c0d
aayushroopchandani <roopchandaniaayush@gmail.com>
2025-03-16 21:04:29 +0530

    Updated feature

---
+++ module1.mjs
@@ -0,0 +1,10 @@
+const a="harry";
+const b="aayush";
+const c="saumya";
+const d="rim";
+
+export default b;
+export {a};
+export {c};
+export {d};
+
---
+++ module2.mjs
@@ -0,0 +1,5 @@
+import ui,{c,a,d} from './module1.mjs';
+console.log(ui);
+console.log(a);
+console.log(c);
+console.log(d);
